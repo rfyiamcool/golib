de59c0be16ecf625d48c8c3f3fcbb82091b52e28
rfyiamcool <rfyiamcool@163.com>
2019-03-27 19:45:09 +0800

    atomic state

---
+++ atomic_state/state.go
@@ -0,0 +1,32 @@
+package atomicState
+
+import (
+	"sync/atomic"
+)
+
+var (
+	setFlag   int32 = 1
+	unsetFlag int32 = 0
+)
+
+func SetFlag(n *int32) bool {
+	return atomic.CompareAndSwapInt32(n, unsetFlag, setFlag)
+}
+
+func IsSetFlag(n *int32) bool {
+	v := atomic.LoadInt32(n)
+	if v == setFlag {
+		return true
+	}
+
+	return false
+}
+
+func IsUnsetFlag(n *int32) bool {
+	v := atomic.LoadInt32(n)
+	if v == setFlag {
+		return false
+	}
+
+	return true
+}
