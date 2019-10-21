2770c3b60569072bdf14859ef720e11b83ac8926
rfyiamcool <rfyiamcool@163.com>
2019-10-21 15:34:45 +0800

    fix test

--- ringqueue/ringqueue_test.go
+++ ringqueue/ringqueue_test.go
@@ -141,11 +141,6 @@ func assertPanics(t *testing.T, name string, f func()) {
 	f()
 }
 
-// General warning: Go's benchmark utility (go test -bench .) increases the number of
-// iterations until the benchmarks take a reasonable amount of time to run; memory usage
-// is *NOT* considered. On my machine, these benchmarks hit around ~1GB before they've had
-// enough, but if you have less than that available and start swapping, then all bets are off.
-
 func BenchmarkQueueSerial(b *testing.B) {
 	q := New()
 	for i := 0; i < b.N; i++ {
@@ -154,17 +149,6 @@ func BenchmarkQueueSerial(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		q.Peek()
 		q.Remove()
-	}
-}
-
-func BenchmarkQueueGet(b *testing.B) {
-	q := New()
-	for i := 0; i < b.N; i++ {
-		q.Add(i)
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		q.Get(i)
 	}
 }
 
