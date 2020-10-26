d03e39e86f2a42a9466c34a2b38965da563fef00
rfyiamcool <rfyiamcool@163.com>
2020-10-26 16:43:04 +0800

    add empty

---
+++ empty/empty.go
@@ -0,0 +1,131 @@
+package empty
+
+import (
+	"reflect"
+)
+
+// apiString is used for type assert api for String().
+type apiString interface {
+	String() string
+}
+
+// apiInterfaces is used for type assert api for Interfaces.
+type apiInterfaces interface {
+	Interfaces() []interface{}
+}
+
+// apiMapStrAny is the interface support for converting struct parameter to map.
+type apiMapStrAny interface {
+	MapStrAny() map[string]interface{}
+}
+
+// IsEmpty checks whether given <value> empty.
+// It returns true if <value> is in: 0, nil, false, "", len(slice/map/chan) == 0,
+// or else it returns false.
+func IsEmpty(value interface{}) bool {
+	if value == nil {
+		return true
+	}
+	switch value := value.(type) {
+	case int:
+		return value == 0
+	case int8:
+		return value == 0
+	case int16:
+		return value == 0
+	case int32:
+		return value == 0
+	case int64:
+		return value == 0
+	case uint:
+		return value == 0
+	case uint8:
+		return value == 0
+	case uint16:
+		return value == 0
+	case uint32:
+		return value == 0
+	case uint64:
+		return value == 0
+	case float32:
+		return value == 0
+	case float64:
+		return value == 0
+	case bool:
+		return value == false
+	case string:
+		return value == ""
+	case []byte:
+		return len(value) == 0
+	case []rune:
+		return len(value) == 0
+	default:
+		// Common interfaces checks.
+		if f, ok := value.(apiString); ok {
+			if f == nil {
+				return true
+			}
+			return f.String() == ""
+		}
+		if f, ok := value.(apiInterfaces); ok {
+			if f == nil {
+				return true
+			}
+			return len(f.Interfaces()) == 0
+		}
+		if f, ok := value.(apiMapStrAny); ok {
+			if f == nil {
+				return true
+			}
+			return len(f.MapStrAny()) == 0
+		}
+		// Finally using reflect.
+		var rv reflect.Value
+		if v, ok := value.(reflect.Value); ok {
+			rv = v
+		} else {
+			rv = reflect.ValueOf(value)
+		}
+		switch rv.Kind() {
+		case reflect.Chan,
+			reflect.Map,
+			reflect.Slice,
+			reflect.Array:
+			return rv.Len() == 0
+
+		case reflect.Func,
+			reflect.Ptr,
+			reflect.Interface,
+			reflect.UnsafePointer:
+			if rv.IsNil() {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// IsNil checks whether given <value> is nil.
+// Note that it might use reflect feature which affects performance a little bit.
+func IsNil(value interface{}) bool {
+	if value == nil {
+		return true
+	}
+	var rv reflect.Value
+	if v, ok := value.(reflect.Value); ok {
+		rv = v
+	} else {
+		rv = reflect.ValueOf(value)
+	}
+	switch rv.Kind() {
+	case reflect.Chan,
+		reflect.Map,
+		reflect.Slice,
+		reflect.Func,
+		reflect.Ptr,
+		reflect.Interface,
+		reflect.UnsafePointer:
+		return rv.IsNil()
+	}
+	return false
+}
